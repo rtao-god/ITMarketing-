8b72f9f671c034de1f318f66eb43f2fdaa9a9cdb
Rtao Dmitry Malinovsky <prostodimka519@gmail.com>
2023-08-23 14:13:06 +0300

    media, jq, form (without binding)

---
+++ js/media.js
@@ -0,0 +1,20 @@
+
+
+
+if ($(window).width() < 767) {
+    $(".headerFlex").css({
+        "display": "block"
+    })
+
+    $(".header").css({
+        "margin": "0px auto 0 20px"
+    })
+} else {
+    $(".headerFlex").css({
+        "display": "flex"
+    })
+
+    $(".header").css({
+        "margin": "0px auto 0 20px"
+    })
+}
