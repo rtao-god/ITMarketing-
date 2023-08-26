d1b99137fd81c570443038b6a2991e957988d5b5
Rtao Dmitry Malinovsky <prostodimka519@gmail.com>
2023-08-26 15:38:13 +0300

    100% js

--- mainPage/FAQ.js
+++ mainPage/FAQ.js
@@ -4,7 +4,7 @@ $('.svgPlus').click(function () {
     if (this.classList.contains('active')) {
         $(this).css("transform", "rotate(45deg)")
         $(this).parent().children("div").children(".FAQResponse").css("display", "block")
-        $(this).parent().css("height", "156px")
+        $(this).parent().css("height", "100%")
     } else {
         $(this).css("transform", "rotate(90deg)")
         $(this).parent().children("div").children(".FAQResponse").css("display", "none")
