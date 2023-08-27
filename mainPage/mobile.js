9ec5d09621be6274056af8c0d20b474522f9a4d1
Rtao Dmitry Malinovsky <prostodimka519@gmail.com>
2023-08-27 14:59:30 +0300

    1001px

--- mainPage/mobile.js
+++ mainPage/mobile.js
@@ -2,4 +2,4 @@
 else (window.screen.width >= 1000) $jq(window).attr("location", "https://rtao-god.github.io/ITMarketing-/mainPage/index.html") */
 
 if (window.screen.width <= 1000) $(window).attr("location", "https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/")
-if (window.screen.width >= 1000) $(window).attr("location", "https://rtao-god.github.io/ITMarketing-/mainPage/index.html")
+if (window.screen.width >= 1001) $(window).attr("location", "https://rtao-god.github.io/ITMarketing-/mainPage/index.html")
