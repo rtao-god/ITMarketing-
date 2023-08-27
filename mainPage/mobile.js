99ddc4b1d207f349a9f7199bea46f5f92a2f94ed
Rtao Dmitry Malinovsky <prostodimka519@gmail.com>
2023-08-27 14:44:43 +0300

    $(window).attr

--- mainPage/mobile.js
+++ mainPage/mobile.js
@@ -1,27 +1,5 @@
-/* if (window.screen.width <= 1000) window.history.pushState("object or string", "Title", "https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/")
-else window.history.pushState("object or string", "Title", 'https://rtao-god.github.io/ITMarketing-/mainPage/index.html/') */
+/* if (window.screen.width <= 1000) $jq(window).attr("location", "https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/")
+else (window.screen.width >= 1000) $jq(window).attr("location", "https://rtao-god.github.io/ITMarketing-/mainPage/index.html") */
 
-
-// if (window.screen.width <= 1000) $(location).prop('href', 'https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/')
-// if (window.screen.width >= 1000) $(location).prop('href', 'https://rtao-god.github.io/ITMarketing-/mainPage/index.html')
-
-
-
-/* if (window.screen.width <= 1000) window.location.href = 'https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/'
-if (window.screen.width >= 1000) window.location.href = 'https://rtao-god.github.io/ITMarketing-/mainPage/index.html' */
-
-
-/* if (window.screen.width <= 1000) {
-    location.assign("https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/")
-    location.replace("https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/")
-}
-if (window.screen.width >= 1000) {
-    location.assign("https://rtao-god.github.io/ITMarketing-/mainPage/index.html")
-    location.replace("https://rtao-god.github.io/ITMarketing-/mainPage/index.html")
-} */
-
-// window.location.href = 'https://rtao-god.github.io/ITMarketing-/mainPage/index.html'
-
-if (window.screen.width <= 1000) $jq(window).attr("location", "https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/")
-if (window.screen.width >= 1000) $jq(window).attr("location", "https://rtao-god.github.io/ITMarketing-/mainPage/index.html")
-
+if (window.screen.width <= 1000) $(window).attr("location", "https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/")
+else (window.screen.width >= 1000) $(window).attr("location", "https://rtao-god.github.io/ITMarketing-/mainPage/index.html")
