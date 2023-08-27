7a0ab594064ce9d11450a00686d15aadb0828736
Rtao Dmitry Malinovsky <prostodimka519@gmail.com>
2023-08-27 14:25:55 +0300

    assign / replace

--- mainPage/mobile.js
+++ mainPage/mobile.js
@@ -7,8 +7,17 @@ else window.history.pushState("object or string", "Title", 'https://rtao-god.git
 
 
 
-if (window.screen.width <= 1000) window.location.href = 'https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/'
-if (window.screen.width >= 1000) window.location.href = 'https://rtao-god.github.io/ITMarketing-/mainPage/index.html'
+/* if (window.screen.width <= 1000) window.location.href = 'https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/'
+if (window.screen.width >= 1000) window.location.href = 'https://rtao-god.github.io/ITMarketing-/mainPage/index.html' */
 
+
+if (window.screen.width <= 1000) {
+    location.assign("https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/")
+    location.replace("https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/")
+}
+if (window.screen.width >= 1000) {
+    location.assign("https://rtao-god.github.io/ITMarketing-/mainPage/index.html")
+    location.replace("https://rtao-god.github.io/ITMarketing-/mainPage/index.html")
+}
 
 // window.location.href = 'https://rtao-god.github.io/ITMarketing-/mainPage/index.html'
