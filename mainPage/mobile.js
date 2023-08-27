386c3f56225fdb035b3c8369ba6cf237ca1d6c1f
Rtao Dmitry Malinovsky <prostodimka519@gmail.com>
2023-08-27 17:17:05 +0300

    $(window).attr

--- mainPage/mobile.js
+++ mainPage/mobile.js
@@ -10,21 +10,10 @@ else (window.screen.width >= 1000) $jq(window).attr("location", "https://rtao-go
 /* window.location.href = "https://rtao-god.github.io/ITMarketing-/mainPage/index.html"
 
 if (window.screen.width <= 1000) {
-    $(window).attr("location", "https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/")
+   
 } else {
     $(window).attr("location", "https://rtao-god.github.io/ITMarketing-/mainPage/index.html")
 }
  */
 
-
-setTimeout(() => {
-    if (window.screen.width <= 1000) window.history.pushState("object or string", "Title", "https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/")
-}, 1000)
-
-
-
-
-
-
-
-
+if (window.screen.width <= 1000) $(window).attr("location", "https://rtao-god.github.io/ITMarketing-/mainPage/mobileVersion/")
